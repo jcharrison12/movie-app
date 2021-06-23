1191e4c08a4e7c8fc3be134600c8cd0b7af60c6e
Jordan <jcharrison12@gmail.com>
2021-06-23 10:14:27 -0500

    Adds restful routes

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,11 +1,33 @@
 class ActorsController < ApplicationController
-  def display_one_actor
-    actor = Actor.find_by(id: 5)
-    render json: actor.as_json
+  def index
+    actors = Actor.all
+    render json: actors.as_json
   end
   def show
     actor_id = params[:id]
     actor = Actor.find(actor_id)
     render json: actor.as_json
+  end
+  def create
+    actor = Actor.new(
+      first_name: params[:first_name], last_name: params[:last_name], known_for: params[:known_for]
+    )
+    actor.save
+    render json: actor.as_json
+  end
+  def update
+    actor_id = params[:id]
+    actor = Actor.find(actor_id)
+    actor.first_name = params[:first_name] || actor.first_name
+    actor.last_name = params[:last_name] || actor.last_name
+    actor.known_for = params[:known_for] || actor.known_for
+    actor.save
+    render json: actor.as_json
+  end
+  def destroy
+    actor_id = params[:id]
+    actor = Actor.find(actor_id)
+    actor.destroy
+    render json: {message: "Actor record destroyed."}
   end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,4 +11,35 @@ class MoviesController < ApplicationController
     movies = Movie.order(:title)
     render json: movies.as_json
   end
+  def index
+    movies = Movie.all
+    render json: movies.as_json
+  end
+  def show
+    movie_id = params[:id]
+    movie = Movie.find(movie_id)
+    render json: movie.as_json
+  end
+  def create
+    movie = Movie.new(
+      title: params[:title], year: params[:year], plot: params[:plot]
+    )
+    movie.save
+    render json: movie.as_json
+  end
+  def update
+    movie_id = params[:id]
+    movie = Movie.find(movie_id)
+    movie.title = params[:title] || movie.title
+    movie.year = params[:year] || movie.year
+    movie.plot = params[:plot] || movie.plot
+    movie.save
+    render json: movie.as_json
+  end
+  def destroy
+    movie_id = params[:id]
+    movie = Movie.find(movie_id)
+    movie.destroy
+    render json: {message: "Movie record destroyed."}
+  end
 end
